441e3a59fef4fd29c76579a7b78c446a8486d097
shkandret <danila.shkandret@yandex.ru>
2023-11-07 10:46:10 +0300

    .

--- hooks/useSidebar.ts
+++ hooks/useSidebar.ts
@@ -1,84 +1,90 @@
 import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from './redux';
-import {
-	filterByTransfer,
-	turnAllTransfers,
-} from '@/store/reducers/ticketsSlice';
+import { filterByTransfer, turnAllTransfers } from '@/store/reducers/ticketsSlice';
 
 const useSidebar = () => {
-	const [checked, setChecked] = useState({
-		all: false,
-		zero: true,
-		one: false,
-		two: false,
-		three: false,
-	});
+  const [checked, setChecked] = useState({
+    all: false,
+    zero: true,
+    one: false,
+    two: false,
+    three: false,
+  });
 
-	const dispatch = useAppDispatch();
-	const { tickets } = useAppSelector((state) => state.tickets);
+  const dispatch = useAppDispatch();
+  const { tickets } = useAppSelector((state) => state.tickets);
 
-	useEffect(() => {
-		if (checked.all) dispatch(turnAllTransfers());
-		if (!checked.all)
-			dispatch(
-				filterByTransfer([
-					{ number: 0, active: checked.zero },
-					{ number: 1, active: checked.one },
-					{ number: 2, active: checked.two },
-					{ number: 3, active: checked.three },
-				])
-			);
-	}, [checked, dispatch, tickets]);
+  useEffect(() => {
+    if (checked.all) dispatch(turnAllTransfers());
+    if (!checked.all)
+      dispatch(
+        filterByTransfer([
+          { number: 0, active: checked.zero },
+          { number: 1, active: checked.one },
+          { number: 2, active: checked.two },
+          { number: 3, active: checked.three },
+        ])
+      );
+    const allChecked = checked.zero && checked.one && checked.two && checked.three;
 
-	const handlerAll = () => {
-		if (checked.all) {
-			setChecked({
-				zero: false,
-				one: false,
-				two: false,
-				three: false,
-				all: false,
-			});
-		} else {
-			setChecked({
-				zero: true,
-				one: true,
-				two: true,
-				three: true,
-				all: true,
-			});
-		}
-	};
+    if (allChecked) {
+      setChecked({
+        ...checked, all:true
+      })
+    }
+  }, [checked, dispatch, tickets]);
 
-	const inputs = [
-		{ id: 'all', label: 'Все', func: () => handlerAll(), checked: checked.all },
-		{
-			id: 'without',
-			label: 'Без пересадок',
-			func: () => setChecked({ ...checked, zero: !checked.zero, all: false }),
-			checked: checked.zero,
-		},
-		{
-			id: '1',
-			label: '1 пересадка',
-			func: () => setChecked({ ...checked, one: !checked.one, all: false }),
-			checked: checked.one,
-		},
-		{
-			id: '2',
-			label: '2 пересадки',
-			func: () => setChecked({ ...checked, two: !checked.two, all: false }),
-			checked: checked.two,
-		},
-		{
-			id: '3',
-			label: '3 пересадки',
-			func: () => setChecked({ ...checked, three: !checked.three, all: false }),
-			checked: checked.three,
-		},
-	];
+  
 
-	return { inputs };
+  const handlerAll = () => {
+    if (checked.all) {
+      setChecked({
+        zero: false,
+        one: false,
+        two: false,
+        three: false,
+        all: false,
+      });
+    } else {
+      setChecked({
+        zero: true,
+        one: true,
+        two: true,
+        three: true,
+        all: true,
+      });
+    }
+  };
+
+  const inputs = [
+    { id: 'all', label: 'Все', func: () => handlerAll(), checked: checked.all },
+    {
+      id: 'without',
+      label: 'Без пересадок',
+      func: () => setChecked({ ...checked, zero: !checked.zero, all: false }),
+      checked: checked.zero,
+    },
+    {
+      id: '1',
+      label: '1 пересадка',
+      func: () => setChecked({ ...checked, one: !checked.one, all: false }),
+      checked: checked.one,
+    },
+    {
+      id: '2',
+      label: '2 пересадки',
+      func: () => setChecked({ ...checked, two: !checked.two, all: false }),
+      checked: checked.two,
+    },
+    {
+      id: '3',
+      label: '3 пересадки',
+      func: () => setChecked({ ...checked, three: !checked.three, all: false }),
+      checked: checked.three,
+    },
+  ];
+
+  return { inputs };
 };
 
 export default useSidebar;
